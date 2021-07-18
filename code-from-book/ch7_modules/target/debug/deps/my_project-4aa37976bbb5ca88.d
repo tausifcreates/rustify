8083582b2b881ab3aab20b46e172e09b0eaf57e6
ino <tawsifriju@gmail.com>
2021-07-18 19:13:37 +0600

    Delete code-from-book/ch7_modules/target directory

--- code-from-book/ch7_modules/target/debug/deps/my_project-4aa37976bbb5ca88.d
+++
@@ -1,5 +0,0 @@
-d:\RIZU\srcs\rust\rust-book-helper\code-from-book\ch7_modules\target\debug\deps\my_project-4aa37976bbb5ca88.rmeta: src\main.rs
-
-d:\RIZU\srcs\rust\rust-book-helper\code-from-book\ch7_modules\target\debug\deps\my_project-4aa37976bbb5ca88.d: src\main.rs
-
-src\main.rs:
