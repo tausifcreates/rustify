c1208c560eb55d36df6cf4cba377dae8feccacff
ino <tawsifriju@gmail.com>
2021-07-24 06:46:53 +0600

    new

--- problems/closest-sum-pair/algorithm/src/lib.rs
+++ problems/closest-sum-pair/algorithm/src/lib.rs
@@ -1,7 +1,89 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1)
+
+pub struct Elements<'list> {
+    list: &'list Vec<i32>,
+    len: usize,
+    desired_sum: i32,
+    pair: Option<(i32, i32)>,
+    init_distance: Option<i32>,
+}
+
+impl<'list> Elements<'list> {
+    pub fn new(list: &'list Vec<i32>, len: usize, desired_sum: i32) -> Self {
+        Elements {
+            list,
+            len,
+            desired_sum,
+            pair: None,
+            init_distance: None,
+        }
+    }
+
+    pub fn find_init_distance(&mut self) -> &mut Self {
+        let list: &Vec<i32> = self.list;
+
+        let len: usize = self.len;
+
+        let desired_sum: i32 = self.desired_sum;
+
+        let highest_sum: i32 = list[len - 1] + list[len - 2];
+
+        let lowest_sum: i32 = list[0] + list[1];
+
+        let avg_sum: i32 = (highest_sum + lowest_sum) / 2;
+
+        let distance: i32;
+
+        if avg_sum - desired_sum <= 0 {
+            distance = desired_sum - lowest_sum;
+        } else {
+            distance = highest_sum - desired_sum;
+        }
+
+        self.init_distance = Some(distance);
+
+        self
+    }
+
+    pub fn find_pair(&mut self) -> &Self {
+        let list: &Vec<i32> = self.list;
+
+        let len: usize = self.len;
+
+        let desired_sum: i32 = self.desired_sum;
+
+        let mut distance = self.init_distance.unwrap();
+
+        let mut left_index: usize = 0;
+
+        let mut right_index: usize = len - 1;
+
+        for _ in 0..(len - 2) + 1 {
+            let temp_sum: i32 = list[left_index] + list[right_index];
+
+            let temp_distance: i32 = desired_sum - temp_sum;
+
+            if temp_distance.abs() < distance.abs() {
+                distance = temp_distance;
+                self.pair = Some((list[left_index], list[right_index]));
+            }
+
+            if temp_distance > 0 {
+                left_index += 1;
+            } else if temp_distance < 0 {
+                right_index -= 1;
+            } else {
+                break;
+            }
+        }
+
+        self
+    }
+
+    pub fn result(&self) -> (i32, i32) {
+        let (first_val, second_val) = self.pair.unwrap();
+        println!("First value: {},\nSecond value: {}", first_val, second_val);
+        (first_val, second_val)
     }
 }
--- problems/closest-sum-pair/find-pair/src/main.rs
+++ problems/closest-sum-pair/find-pair/src/main.rs
@@ -1,3 +1,126 @@
+use algorithm::Elements;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Press q to quit from the program any time.");
+
+    let steps: u32 = loop {
+        let mut steps: String = String::new();
+
+        println!("Number of turns:");
+
+        match io::stdin().read_line(&mut steps) {
+            Ok(_) => {}
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        }
+
+        if steps.trim() == "q" {
+            return;
+        }
+
+        let steps: u32 = match steps.trim().parse() {
+            Ok(num) => num,
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
+
+        break steps;
+    };
+
+    for _ in 1..steps + 1 {
+        let mut list: Vec<i32> = Vec::new();
+
+        let mut len = String::new();
+
+        let mut desired_sum: String = String::new();
+
+        println!("Desired sum value:");
+
+        match io::stdin().read_line(&mut desired_sum) {
+            Ok(_) => {}
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        }
+
+        if desired_sum.trim() == "q" {
+            return;
+        }
+
+        let desired_sum: i32 = match desired_sum.trim().parse() {
+            Ok(num) => num,
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
+
+        println!("Size of list: ");
+
+        match io::stdin().read_line(&mut len) {
+            Ok(_) => {}
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        }
+
+        if len.trim() == "q" {
+            return;
+        }
+
+        let len: usize = match len.trim().parse() {
+            Ok(num) => num,
+
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
+
+        println!("Enter the numbers in list:");
+
+        for _ in 0..len {
+            let mut input_elem: String = String::new();
+
+            let input_elem: String = match io::stdin().read_line(&mut input_elem) {
+                Ok(_) => input_elem,
+
+                Err(e) => {
+                    println!("{}", e);
+                    continue;
+                }
+            };
+
+            let input_elem: i32 = match input_elem.trim().parse() {
+                Ok(num) => num,
+
+                Err(e) => {
+                    println!("{}", e);
+                    continue;
+                }
+            };
+
+            list.push(input_elem);
+        }
+
+        list.sort();
+
+        let mut elements = Elements::new(&list, len, desired_sum);
+
+        elements.find_init_distance().find_pair().result();
+
+        //println!("{:?}", list);
+    }
 }
