bb8c2e97445f5a60ba9fe9bfba91e03ca036c80c
Inoshy <tawsifriju@gmail.com>
2021-07-28 07:55:42 +0600

    new

--- problems/elements-frequency/src/lib.rs
+++ problems/elements-frequency/src/lib.rs
@@ -1,9 +1,42 @@
+//! # elements frequency
+//!
+//! Finds frequency of the unique elements present in the list.
+//! This lbrary can work with any types that implement `Clone`.
+//! So it is expected it will work with `String`, `&str`, `i32` etc.
+//!
+//! ### Quick Example
+//! ```
+//! use elements_frequency::Elements;
+//!
+//! let list: Vec<i32> = vec![1, 1, -6, 2, 6, 2, 7, 1];
+//!
+//! let mut elements = Elements::new(list);
+//!
+//! let result:&Vec<(T, u32)> = elements.find_arbitrary().update_ordered().result();
+//!
+//! let expected: &Vec<(i32, u32)> = &vec![(1, 3), (-6, 1), (2, 2), (6, 1), (7, 1)];
+//!
+//! assert_eq!(expected, result);
+//! ```
+
 use std::{collections::HashMap, hash::Hash};
+
+#[derive(PartialEq)]
+pub struct Row<U> {
+    pub element: U,
+    pub frequency: u32,
+}
+
+impl<U> Row<U> {
+    pub fn new(element: U, frequency: u32) -> Self {
+        Row { element, frequency }
+    }
+}
 
 pub struct Elements<'list, T> {
     list: &'list Vec<T>,
-    arbitrary_table: HashMap<T, u32>,
-    ordered_table: Vec<(T, u32)>,
+    unordered_hash: HashMap<T, u32>,
+    ordered_table: Vec<Row<T>>,
 }
 
 impl<'list, T> Elements<'list, T>
@@ -13,27 +46,27 @@ where
     pub fn new(list: &'list Vec<T>) -> Self {
         Elements {
             list,
-            arbitrary_table: HashMap::new(),
+            unordered_hash: HashMap::new(),
             ordered_table: Vec::new(),
         }
     }
 
-    pub fn arbitrary_frequency_table(&mut self) -> &mut Self {
+    pub fn hash_unorderly(&mut self) -> &mut Self {
         let list: &Vec<T> = self.list;
 
-        let arbitrary_table: &mut HashMap<T, u32> = &mut self.arbitrary_table;
+        let unordered_hash: &mut HashMap<T, u32> = &mut self.unordered_hash;
 
-        let ordered_table: &mut Vec<(T, u32)> = &mut self.ordered_table;
+        let ordered_table: &mut Vec<Row<T>> = &mut self.ordered_table;
 
         for i in list {
-            match arbitrary_table.get_mut(i) {
+            match unordered_hash.get_mut(i) {
                 Some(val) => {
                     *val += 1;
                 }
 
                 None => {
-                    arbitrary_table.insert((*i).clone(), 1);
-                    ordered_table.push(((*i).clone(), 0));
+                    unordered_hash.insert((*i).clone(), 1);
+                    ordered_table.push(Row::new((*i).clone(), 0))
                 }
             }
         }
@@ -42,20 +75,20 @@ where
     }
 
     pub fn update_ordered(&mut self) -> &Self {
-        let arbitrary_table: &mut HashMap<T, u32> = &mut self.arbitrary_table;
+        let unordered_hash: &mut HashMap<T, u32> = &mut self.unordered_hash;
 
-        let ordered_table: &mut Vec<(T, u32)> = &mut self.ordered_table;
+        let ordered_table: &mut Vec<Row<T>> = &mut self.ordered_table;
 
-        for (elem, frequency) in ordered_table.iter_mut() {
-            let val = arbitrary_table.get(elem).unwrap();
-            *frequency += *val;
+        for row in ordered_table.iter_mut() {
+            let val: &u32 = unordered_hash.get(&row.element).unwrap();
+            row.frequency += *val;
         }
 
         self
     }
 
-    pub fn result(&self) -> &Vec<(T, u32)> {
-        let ordered_table: &Vec<(T, u32)> = &self.ordered_table;
+    pub fn result(&self) -> &Vec<Row<T>> {
+        let ordered_table: &Vec<Row<T>> = &self.ordered_table;
         ordered_table
     }
 }
--- problems/elements-frequency/tests/integration_test.rs
+++ problems/elements-frequency/tests/integration_test.rs
@@ -1,30 +1,136 @@
-use elements_frequency::Elements;
+use elements_frequency::{Elements, Row};
 
 #[cfg(test)]
 mod tests {
-    use std::{fmt::Debug, hash::Hash};
+    use std::{fmt::Debug, hash::Hash, u32};
 
     use super::*;
 
-    fn setup<T>(list: &Vec<T>, expected: &Vec<(T, u32)>) -> () 
-    where T: Clone + Hash + Eq + Debug
+    fn setup<T>(list: &Vec<T>, expected: &Vec<(T, u32)>) -> ()
+    where
+        T: Clone + Hash + Eq + Debug,
     {
-        let mut elements = Elements::new(list);
+        let mut elements: Elements<T> = Elements::new(list);
 
-        let result = elements
-            .arbitrary_frequency_table()
-            .update_ordered()
-            .result();
-        
-        assert_eq!(expected, result);
+        let frequency_table: &Vec<Row<T>> = elements.hash_unorderly().update_ordered().result();
+
+        let mut i: usize = 0;
+
+        for row in frequency_table.iter() {
+            let (element, frequency) = ((row.element).clone(), row.frequency);
+
+            assert_eq!(expected[i].0, element);
+            assert_eq!(expected[i].1, frequency);
+
+            i += 1;
+        }
     }
 
     #[test]
-    fn positive_random() {
+    fn random_int_1() {
         let list: Vec<i32> = vec![1, 1, -6, 2, 6, 2, 7, 1];
 
-        let expected: &Vec<(i32, u32)> = &vec![(1, 3), (-6, 1), (2, 2), (6, 1), (7, 1)];
+        let expected: Vec<(i32, u32)> = vec![(1, 3), (-6, 1), (2, 2), (6, 1), (7, 1)];
 
-        setup(&list, expected);
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn random_int_2() {
+        let list: Vec<i32> = vec![-5, 11, 4, 4, -5, -7, 11];
+
+        let expected: Vec<(i32, u32)> = vec![(-5, 2), (11, 2), (4, 2), (-7, 1)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn positive_int_1() {
+        let list: Vec<i32> = vec![1, 3, 3, 1, 2, 3, 2, 1];
+
+        let expected: Vec<(i32, u32)> = vec![(1, 3), (3, 3), (2, 2)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn positive_int_2() {
+        let list: Vec<i32> = vec![2, 2, 2, 2, 2, 2];
+
+        let expected: Vec<(i32, u32)> = vec![(2, 6)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn negative_int_1() {
+        let list: Vec<i32> = vec![-5, -2, -7, -2, -7, -7, -2, -5, -8];
+
+        let expected: Vec<(i32, u32)> = vec![(-5, 2), (-2, 3), (-7, 3), (-8, 1)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn negative_int_2() {
+        let list: Vec<i32> = vec![-3, -3, -3, -3, -3, -3];
+
+        let expected: Vec<(i32, u32)> = vec![(-3, 6)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn slice_1() {
+        let list: Vec<&str> = vec!["hi", "who", "me", "who", "me", "hi"];
+
+        let expected: Vec<(&str, u32)> = vec![("hi", 2), ("who", 2), ("me", 2)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn slice_2() {
+        let list: Vec<&str> = vec!["hi", "hi", "hi", "hi", "hi", "hi"];
+
+        let expected: Vec<(&str, u32)> = vec![("hi", 6)];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn string_1() {
+        let list: Vec<String> = vec![
+            String::from("hey"),
+            String::from("you"),
+            String::from("hey"),
+            String::from("stop"),
+            String::from("you"),
+            String::from("stop"),
+        ];
+
+        let expected: Vec<(String, u32)> = vec![
+            (String::from("hey"), 2),
+            (String::from("you"), 2),
+            (String::from("stop"), 2),
+        ];
+
+        setup(&list, &expected);
+    }
+
+    #[test]
+    fn string_2() {
+        let list: Vec<String> = vec![
+            String::from("hi"),
+            String::from("hi"),
+            String::from("hi"),
+            String::from("hi"),
+            String::from("hi"),
+            String::from("hi"),
+        ];
+
+        let expected: Vec<(String, u32)> = vec![(String::from("hi"), 6)];
+
+        setup(&list, &expected);
     }
 }
