ea99be85e11c5c388ec3a6926381d48f5ea5221d
Inoshy <tawsifriju@gmail.com>
2021-07-30 18:49:15 +0600

    new

--- problems/sorted-rotated/src/lib.rs
+++ problems/sorted-rotated/src/lib.rs
@@ -2,18 +2,18 @@ pub struct Elements<'list> {
     list: &'list [i32],
     len: usize,
     pivot: Option<usize>,
-    //desired: i32,
-    //result: Option<usize>,
+    desired: i32,
+    result: Option<usize>,
 }
 
 impl<'list> Elements<'list> {
-    pub fn new(list: &'list [i32], _desired: i32) -> Self {
+    pub fn new(list: &'list [i32], desired: i32) -> Self {
         Elements {
             list,
             len: list.len(),
             pivot: None,
-            //desired,
-            //result: None,
+            desired,
+            result: None,
         }
     }
 
@@ -46,7 +46,57 @@ impl<'list> Elements<'list> {
         self
     }
 
+    pub fn find_desired(&mut self) -> &Self {
+        let list: &[i32] = self.list;
+
+        let len: usize = self.len;
+
+        let pivot: Option<usize> = self.pivot;
+
+        let desired: i32 = self.desired;
+
+        let mut left: usize = 0;
+
+        let mut right: usize = len - 1;
+
+        let mut distance: Option<usize> = None;
+
+        match pivot {
+            Some(pivot_idx) => {
+                if desired > list[left] {
+                    right = pivot_idx;
+                    distance = Some(pivot_idx - left);
+                } else if desired < list[left] {
+                    left = pivot_idx;
+                    distance = Some(right - pivot_idx);
+                }
+            }
+            None => {
+                distance = Some(right - left);
+            }
+        };
+
+        let turns: usize = (distance.unwrap() as f64).log(2.0).abs() as usize + 1;
+
+        for _ in 0..turns {
+            let mid: usize = (left + right) / 2;
+
+            if list[mid] == desired {
+                self.result = Some(mid);
+                break;
+            }
+
+            if list[mid] < desired {
+                left = mid;
+            } else if list[mid] > desired {
+                right = mid;
+            }
+        }
+
+        self
+    }
+
     pub fn result(&self) -> Option<usize> {
-        self.pivot
+        self.result
     }
 }
--- problems/sorted-rotated/tests/integration_test.rs
+++ problems/sorted-rotated/tests/integration_test.rs
@@ -2,27 +2,33 @@
 mod tests {
     use sorted_rotated::Elements;
 
-    fn setup(list: &Vec<i32>, expected: Option<usize>) {
-        let mut elem = Elements::new(list, 0);
-        let result = elem.find_pivot().result();
+    fn setup(list: &Vec<i32>, desired: i32, expected: Option<usize>) {
+        let mut elem: Elements = Elements::new(list, desired);
+
+        let result: Option<usize> = elem.find_pivot().find_desired().result();
+        
         assert_eq!(expected, result);
     }
 
     #[test]
     fn no_pivot() {
-        let list = vec![1, 2, 3, 4, 5];
+        let list: Vec<i32> = vec![1, 2, 3, 4, 5];
 
-        let expected: Option<usize> = None;
+        let desired: i32 = 2;
 
-        setup(&list, expected);
+        let expected: Option<usize> = Some(1);
+
+        setup(&list, desired, expected);
     }
 
     #[test]
     fn pivot_random() {
-        let list = vec![5, 6, 1, 2, 3, 4];
+        let list: Vec<i32> = vec![5, 6, 1, 2, 3, 4];
 
-        let expected: Option<usize> = Some(1);
+        let desired: i32 = 2;
 
-        setup(&list, expected);
+        let expected: Option<usize> = Some(3);
+
+        setup(&list, desired, expected);
     }
 }
