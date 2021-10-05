ca895b8c384aefb4575c9da8a37017c2da400a4f
ino <tawsifriju@gmail.com>
2021-10-05 21:35:41 +0600

    fix docs

--- problems/elements-frequency/src/lib.rs
+++ problems/elements-frequency/src/lib.rs
@@ -1,33 +1,19 @@
-//! # elements_frequency
-//!
-//! Finds frequency table of the unique elements present in the list.
-//! In the table the elements come in "First come first serve" manner,
-//! namingly the order they appear in the list.
-//!
-//! This lbrary can work with any types that implement `Clone`.
-//! So it is expected to work with Strings, slices, integers etc.
-//!
-//! # Quick Start
-//! ```
-//! use elements_frequency::interface::Elements;
-//!
-//! let list = vec!["hi", "who", "me", "who", "me"];
-//! 
-//! // Or you can use an array instead:
-//! let list = ["hi", "who", "me", "who", "me"];
-//! 
-//! let mut elements = Elements::new(&list);
-//!
-//! let table = elements.hash_couple().update_order().result();
-//!    
-//! println!("{:?}", table);
-//!
-//! //
-//! // [
-//! //    Row { element: "hi", frequency: 1 }, 
-//! //    Row { element: "who", frequency: 2 }, 
-//! //    Row { element: "me", frequency: 2 },
-//! // ]   
-//! //
-//! ```
+/// Examples
+/// ```
+/// use elements_frequency::interface::frequency_finder;
+/// 
+/// fn main () {
+///     let myList = [1, 1, -6, 2, 6, 2, 7, 1];
+/// 
+///     let myThreads = 6;
+/// 
+///     let frequency_map = frequency_finder(&myList, myThreads);
+/// 
+///     println!("{:?}", frequency_map);
+///     
+///     // Output:
+///     // {6: 1, -6: 1, 2: 2, 7: 1, 1: 3}
+/// }
+/// 
+/// ```
 pub mod interface;
